669cd4ddd2073b02ec215b0354693f4861e1061e
srstack <srstack0203@gmail.com>
2026-07-31 07:13:17 +0800

    style(libterm): collapse nested if in unix shell example

--- otty-libterm/examples/unix_shell.rs
+++ otty-libterm/examples/unix_shell.rs
@@ -165,16 +165,16 @@ mod unix_shell {
 
         write!(out, "\x1b[0m")?;
 
-        if view.cursor.shape != otty_escape::CursorShape::Hidden {
-            if let Some(cursor) = otty_libterm::surface::point_to_viewport(
+        if view.cursor.shape != otty_escape::CursorShape::Hidden
+            && let Some(cursor) = otty_libterm::surface::point_to_viewport(
                 view.display_offset,
                 view.cursor.point,
-            ) {
-                let row = cursor.line;
-                let col = cursor.column.0;
-                if row < rows && col < cols {
-                    write!(out, "\x1b[{};{}H\x1b[?25h", row + 1, col + 1)?;
-                }
+            )
+        {
+            let row = cursor.line;
+            let col = cursor.column.0;
+            if row < rows && col < cols {
+                write!(out, "\x1b[{};{}H\x1b[?25h", row + 1, col + 1)?;
             }
         }
 
